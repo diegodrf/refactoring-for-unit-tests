8eecf602ec640ac1cc3976ffbc7442dbfd491f24
Diego Faria <diego.rdfaria@gmail.com>
2022-10-10 00:58:36 -0300

    Add Tests for Commands

---
+++ Store.Tests/Commands/CreateOrderCommandTests.cs
@@ -0,0 +1,21 @@
+﻿using Store.Domain.Commands;
+
+namespace Store.Tests.Commands
+{
+    [TestClass]
+    public class CreateOrderCommandTests
+    {
+        [TestMethod]
+        [TestCategory("Commands")]
+        public void Dado_um_commando_invalido_o_pedido_nao_deve_ser_gerado()
+        {
+            var customerName = string.Empty;
+            var command = new CreateOrderCommand(customerName, "11111111");
+            command.AddOrderItemCommand(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.AddOrderItemCommand(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Validate();
+
+            Assert.IsFalse(command.IsValid);
+        }
+    }
+}
