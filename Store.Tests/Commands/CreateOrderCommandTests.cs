d85f0050359bd168a22184faaf854c5aed88d635
Diego Faria <diego.rdfaria@gmail.com>
2022-10-10 02:41:50 -0300

    Fix var names

--- Store.Tests/Commands/CreateOrderCommandTests.cs
+++ Store.Tests/Commands/CreateOrderCommandTests.cs
@@ -9,8 +9,8 @@ namespace Store.Tests.Commands
         [TestCategory("Commands")]
         public void Dado_um_commando_invalido_o_pedido_nao_deve_ser_gerado()
         {
-            var customerName = string.Empty;
-            var command = new CreateOrderCommand(customerName, "11111111");
+            var customerDocumentNumber = string.Empty;
+            var command = new CreateOrderCommand(customerDocumentNumber, "11111111");
             command.AddOrderItemCommand(new CreateOrderItemCommand(Guid.NewGuid(), 1));
             command.AddOrderItemCommand(new CreateOrderItemCommand(Guid.NewGuid(), 1));
             command.Validate();
